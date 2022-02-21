435d96f8f722a1d66f0c83700f52da61737afa52
lipa88-mk <lipa88@mail.ru>
2022-02-21 18:32:30 +0300

    добавляет шапку

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
       './src/**/*.css',
   ],
   theme: {
-    extend: {},
+    colors: { 
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'gray-dark': '#d6d6d6',
+      'gray-light': '#f8f8f8',
+      'black': '#000000',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    fontSize: { 
+      base: ['14px', '16px'],
+    },
+    extend: {
+      gridTemplateColumns: {
+        'header': 'minmax(max-content, 1fr) 70px minmax(max-content, 1fr)',
+      }
+    },
   },
   plugins: [],
 }
